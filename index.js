55ddca31028bd05eab3bcfb140d93e410373ab27
Grigorii Burdin <birdingri@gmail.com>
2017-04-10 12:13:55 +0300

    test

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var express = require("express"),
 	pg = require("pg"),
 	cors = require("cors"),
-	bodyParser = require("body-parser");
+	bodyParser = require("body-parser"),
+	cloudinary = require("cloudinary"),
+	cloudinaryStorage = require("multer-storage-cloudinary");
 
 var app = express();
+
+var storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: '',
+  allowedFormats: ['jpg', 'png'],
+});
+
+var parser = multer({ storage: storage });
 
 app.set('port', (process.env.PORT || 3000));
 
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get('/', function(req, res) {
+  res.send('<form method="post" enctype="multipart/form-data">'
+    + '<p>Public ID: <input type="text" name="title"/></p>'
+    + '<p>Image: <input type="file" name="image"/></p>'
+    + '<p><input type="submit" value="Upload"/></p>'
+    + '</form>');
+});
+
+app.post('/', parser.single('image'), function(req, res) {
+	res.send("Done");
+});
 
 app.get('/categories', function(req, res) {
 	pg.connect(process.env.DATABASE_URL, function(err, client, done){
