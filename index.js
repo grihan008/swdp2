82ede021a55aa2699f768cd8b1d30770b1f92101
Grigorii Burdin <birdingri@gmail.com>
2017-04-22 17:29:05 +0300

    42

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ app.post('/upload_step', parser.single('image'), function(req, res) {
 				res.send("Error");
 			}
 			else{
-				//res.redirect("/adminskill/"+req.body.skillid);
+				res.redirect("/test");
 			}
 		});
 	});	
