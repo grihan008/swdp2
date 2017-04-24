a6cac0458a34af72af74e303bb0ae51216128a99
Grigorii Burdin <birdingri@gmail.com>
2017-04-24 20:10:07 +0300

    42

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/login', function(req,res){
 				if (loggedin){
 					sess.userLoggedIn=true;
 					sess.userID=id;
-					res.json({id:id});
+					res.json(user);
 				}
 				else{
 					req.session.destroy(function(err) {
