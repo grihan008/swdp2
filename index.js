45fdff5911db9bac457476230f89bebfa5e6a8e6
Grigorii Burdin <birdingri@gmail.com>
2017-04-24 19:04:12 +0300

    42

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use(cors());
 var sess;
 
 app.get('/login', function(req,res){
-	res.json({ id: 1 });
+	res.json({ id: 2 });
 	// sess=req.session;
 	// if (sess.userLoggedIn){
 	// 	res.json(sess.userID);
